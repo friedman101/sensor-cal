7a415f71cae808af18b7eec1427a69d27dc6862f
David Friedman <dhfriedman@gmail.com>
2016-07-01 21:43:41 -0700

    boilerplate removal

--- src/sensor_cal.c
+++ src/sensor_cal.c
@@ -16,13 +16,36 @@ x1 = x[i][0]; \
 x2 = x[i][1]; \
 x3 = x[i][2];
 
+#define PACK_B \
+gsl_matrix_set(B, 0, 0, a1); \
+gsl_matrix_set(B, 1, 0, a2); \
+gsl_matrix_set(B, 2, 0, a3); \
+gsl_matrix_set(B, 3, 0, a4); \
+gsl_matrix_set(B, 4, 0, a5); \
+gsl_matrix_set(B, 5, 0, a6); \
+gsl_matrix_set(B, 6, 0, b1); \
+gsl_matrix_set(B, 7, 0, b2); \
+gsl_matrix_set(B, 8, 0, b3); \
+
+#define UNPACK_B \
+A[0][0] = gsl_matrix_get(B, 0, 0); \
+A[0][1] = gsl_matrix_get(B, 1, 0); \
+A[0][2] = gsl_matrix_get(B, 2, 0); \
+A[1][1] = gsl_matrix_get(B, 3, 0); \
+A[1][2] = gsl_matrix_get(B, 4, 0); \
+A[2][2] = gsl_matrix_get(B, 5, 0); \
+b[0] = gsl_matrix_get(B, 6, 0); \
+b[1] = gsl_matrix_get(B, 7, 0); \
+b[2] = gsl_matrix_get(B, 8, 0);
+
+
 double calc_residual(double (*x)[3], unsigned int x_len,
         double x_mag, double A[3][3], double b[3], gsl_matrix *residual) {
 
-    SPLIT_VARS
+    SPLIT_VARS;
     double e = 0;
     for (unsigned int i = 0; i < x_len; i++) {
-        SPLIT_INPUTS
+        SPLIT_INPUTS;
         double p1 = a6*x3+b3;
         double p2 = a5*x3+a4*x2+b2;
         double p3 = a3*x3+a2*x2+a1*x1+b1;
@@ -41,6 +64,10 @@ unsigned int cal(double (*x)[3], unsigned int x_len,
         double x_mag, double A[3][3], double b[3],
         unsigned int max_iter, double tol) {
 
+    A[1][0] = 0;
+    A[2][0] = 0;
+    A[2][1] = 0;
+
     gsl_permutation *p = gsl_permutation_alloc(9);
     gsl_matrix *J = gsl_matrix_alloc(x_len, 9);
     gsl_matrix *Jt = gsl_matrix_alloc(9, x_len);
@@ -58,27 +85,18 @@ unsigned int cal(double (*x)[3], unsigned int x_len,
         if (e < tol)
             break;
 
-        SPLIT_VARS
-        gsl_matrix_set(B, 0, 0, a1);
-        gsl_matrix_set(B, 1, 0, a2);
-        gsl_matrix_set(B, 2, 0, a3);
-        gsl_matrix_set(B, 3, 0, a4);
-        gsl_matrix_set(B, 4, 0, a5);
-        gsl_matrix_set(B, 5, 0, a6);
-        gsl_matrix_set(B, 6, 0, b1);
-        gsl_matrix_set(B, 7, 0, b2);
-        gsl_matrix_set(B, 8, 0, b3);
+        SPLIT_VARS;
         for (unsigned int i = 0; i < x_len; i++) {
-            SPLIT_INPUTS
+            SPLIT_INPUTS;
             double J_mat[9] = {2*x1*(a3*x3+a2*x2+a1*x1+b1),
-            2*x2*(a3*x3+a2*x2+a1*x1+b1), 
-            2*x3*(a3*x3+a2*x2+a1*x1+b1),
-            2*x2*(a5*x3+a4*x2+b2),
-            2*x3*(a5*x3+a4*x2+b2),
-            2*x3*(a6*x3+b3),
-            2*(a3*x3+a2*x2+a1*x1+b1),
-            2*(a5*x3+a4*x2+b2),
-            2*(a6*x3+b3)};
+                2*x2*(a3*x3+a2*x2+a1*x1+b1),
+                2*x3*(a3*x3+a2*x2+a1*x1+b1),
+                2*x2*(a5*x3+a4*x2+b2),
+                2*x3*(a5*x3+a4*x2+b2),
+                2*x3*(a6*x3+b3),
+                2*(a3*x3+a2*x2+a1*x1+b1),
+                2*(a5*x3+a4*x2+b2),
+                2*(a6*x3+b3)};
             for (unsigned int k = 0; k < 9; k++)
                 gsl_matrix_set(J, i, k, J_mat[k]);
         }
@@ -90,17 +108,9 @@ unsigned int cal(double (*x)[3], unsigned int x_len,
         gsl_linalg_LU_invert(JtJ, p, JtJinv);
         gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, JtJinv, Jt, 0.0, JtJinvJt); 
         gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, JtJinvJt, residual, 0.0, JtJinvJtr); 
+        PACK_B;
         gsl_matrix_add(B, JtJinvJtr);
-
-        A[0][0] = gsl_matrix_get(B, 0, 0);
-        A[0][1] = gsl_matrix_get(B, 1, 0);
-        A[0][2] = gsl_matrix_get(B, 2, 0);
-        A[1][1] = gsl_matrix_get(B, 3, 0);
-        A[1][2] = gsl_matrix_get(B, 4, 0);
-        A[2][2] = gsl_matrix_get(B, 5, 0);
-        b[0] = gsl_matrix_get(B, 6, 0);
-        b[1] = gsl_matrix_get(B, 7, 0);
-        b[2] = gsl_matrix_get(B, 8, 0);
+        UNPACK_B;
 
     }
 
